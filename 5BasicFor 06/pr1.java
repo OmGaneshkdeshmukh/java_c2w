c56e228e0cb5ad43c994efde837a85b528d3831a
OmGanesh2426 <99478501+OmGaneshkdeshmukh@users.noreply.github.com>
2024-06-10 14:19:17 +0530

    Add files via upload
    Basic For Loop

---
+++ 5BasicFor 06/pr1.java
@@ -0,0 +1,8 @@
+class basicfor{
+	public static void main(String[] args){
+		for (int i=1; i<=10; i++){
+			System.out.print(i + ",");
+		}
+	}
+}
+
---
+++ 5BasicFor 06/pr6.java
@@ -0,0 +1,8 @@
+class P6{
+	public static void main(String[] artgs){
+		for (int i=80; i>=10; i--){
+			System.out.println(i);
+		}
+	}
+}
+
